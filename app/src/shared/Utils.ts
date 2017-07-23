b2abdb884be2be9e3652d80d6c73cefa796ca116
Michael Nitze <mn@pluto.fritz.box>
2017-07-23 02:39:56 +0200

    Source code documentation.

--- app/src/shared/Utils.ts
+++ app/src/shared/Utils.ts
@@ -1,10 +1,15 @@
 import { $FSE, $Path } from "./Modules";
 
 /**
- *
- * @param value T
- * @param defaultValue T
- * @returns T
+ * Check a given value if it has the same type as `defaultValue`.
+ * If the types don't match, a default value is returned. This function is
+ * useful if potentially unsafe values or unknown types (for example from
+ * a JSON object) have to be set to a specific type (and default value).
+ * *Note:* Using generics instead of `any` enables the TypeScript compiler
+ * to detect wrong uses like `normalize("string", true)`.
+ * @param value The value to be checked.
+ * @param defaultValue The default value to be returned if `value` is of different type than `defaultValue`.
+ * @returns A default value or the initial given value.
  */
 export function normalize<T>(value: T, defaultValue: T): T {
     if (Array.isArray(value)) {
@@ -29,11 +34,12 @@ export function normalize<T>(value: T, defaultValue: T): T {
     } else if (typeof value === typeof defaultValue) {
         return value;
     }
-    return defaultValue as T;
+    return defaultValue;
 }
 
 /**
- *
+ * An object conatining the directories and files
+ * from a directory listing, separated in arrays.
  */
 export interface DirectoryListing {
     Directories: string[];
@@ -41,10 +47,10 @@ export interface DirectoryListing {
 }
 
 /**
- *
- * @param directory
- * @param outListing
- * @param recursive
+ * Fill a DirectoryListing object with the contents from `directory`.
+ * @param directory The path for which the directory listing should be executed.
+ * @param outListing A DirectoryListing object which receives the result of reading the contents of `directory`.
+ * @param recursive True if reading the directory should be executed recursively.
  */
 export function fillDirectoryListing(directory: string, outListing: DirectoryListing, recursive?: boolean): void {
     const entries: string[] = $FSE.readdirSync(directory);
@@ -63,10 +69,10 @@ export function fillDirectoryListing(directory: string, outListing: DirectoryLis
 }
 
 /**
- *
- * @param directory
- * @param recursive
- * @returns DirectoryListing
+ * Read the contents of a directory.
+ * @param directory The path for which the directory listing should be executed.
+ * @param recursive True if reading the directory should be executed recursively.
+ * @returns A DirectoryListing object which contains the result of reading the contents of `directory`.
  */
 export function getDirectoryListing(directory: string, recursive?: boolean): DirectoryListing {
     const listing: DirectoryListing = { Directories: [], Files: [] };
