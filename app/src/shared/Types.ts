179134430ac557df140a90e24a6062044eb28a54
Michael Nitze <michael.nitze@michael-nitze.de>
2022-05-23 19:45:53 +0200

    Added 'type-safe' any.

--- app/src/shared/Types.ts
+++ app/src/shared/Types.ts
@@ -3,3 +3,8 @@
  * Shorthand for `Record<string, any>`.
  */
 export type AnyObject = Record<string, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
+
+/**
+ * Explicit type for `any` (silencing the linter).
+ */
+export type AnyType = any; // eslint-disable-line @typescript-eslint/no-explicit-any
